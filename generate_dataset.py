e29fe37e59e26975e1ca028b49da726df383e5c6
imanfs <imanfsanai@gmail.com>
2025-03-17 17:44:52 +0000

    added argparsing

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,9 +1,9 @@
 from utils import *
 import pandas as pd
+import argparse
 
 def process_sanctions_data(df):
     processed_df = df.copy()
-    print(processed_df.columns)
     processed_df['Full_Name'] = processed_df.apply(create_full_name, axis=1)
     processed_df['Full_Name_Standardized'] = clean_name(processed_df['Full_Name'])
     processed_df["Associated Countries"] = processed_df.apply(create_associated_countries, axis=1)
@@ -48,14 +48,25 @@ def create_final_dataset(df):
     return dataset
 
 if __name__ == "__main__":
-    sanctions_data = pd.read_csv('ConList.csv',skiprows=1)
+    
+    def parse_arguments():
+        parser = argparse.ArgumentParser(description='Process sanctions data.')
+        parser.add_argument('--input', type=str, help='Path to the input CSV file',default='ConList.csv')
+        parser.add_argument('--output_dataset', type=str, help='Path to save the final dataset CSV file',default='final_sanctions_dataset.csv')
+        parser.add_argument('--output_summary', type=str, help='Path to save the data quality assessment CSV file',default='data_quality_assessment.csv')
+        return parser.parse_args()
+
+    args = parse_arguments()
+
+    sanctions_data = pd.read_csv(args.input, skiprows=1)
     processed_sanctions_data = process_sanctions_data(sanctions_data)
     summary = generate_summary(sanctions_data)
     final_dataset = create_final_dataset(processed_sanctions_data)
-    
+    print("Dataset generation completed successfully!")
     # Save the final dataset to a CSV file
-    final_dataset.to_csv('final_sanctions_dataset.csv', index=False)
-    
+    final_dataset.to_csv(args.output_dataset, index=False)
+
     # Save the data quality assessment to a CSV file
-    pd.DataFrame([summary]).to_csv('data_quality_assessment.csv', index=False)
+    pd.DataFrame([summary]).to_csv(args.output_summary, index=False)
+    print("Data quality summary saved successfully!")
     
